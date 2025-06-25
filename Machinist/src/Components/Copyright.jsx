17dd8c1e681659a6b11177dfa476ea2ed6ae8a7c
Hafiza <hafizaaqsa944@gmail.com>
2025-06-25 23:12:44 +0500

    initial commit

---
+++ Machinist/src/Components/Copyright.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.2.PNG";
+
+export default function Copyright() {
+  return (
+    <div className="flex flex-col sm:flex-row justify-between items-center bg-gray-100 px-6 py-4 text-center sm:text-left">
+      <p className="text-sm text-gray-600 mb-2 sm:mb-0">
+        © 2025 Machinist.com (Pvt) Ltd. All rights reserved.
+      </p>
+      <img
+        src={logo}
+        alt="Machinist Company Logo"
+        className="h-7 w-auto"
+      />
+    </div>
+  );
+}
