0aecb6ade63c8910b4ff1cdfa518042fd3115f2d
Hafiza <hafizaaqsa944@gmail.com>
2025-06-27 14:25:35 +0500

    updated

--- Machinist/src/App.jsx
+++ Machinist/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./Components/Footer.jsx";
 import Copyright from "./Components/Copyright.jsx";
 import Home from "./Pages/Home.jsx";
 import Services from "./Pages/Service.jsx";
-import Cleaning from "./pages/Cleaning.jsx";
+import Cleaning from "./Pages/Cleaning.jsx";
 import PersonalCare from "./Pages/PersonalCare.jsx";
 import Info from "./Pages/Info.jsx";
 import Areas from "./Pages/Areas.jsx";
@@ -24,7 +24,7 @@ export default function App() {
         <Route path="/personal-care" element={<PersonalCare />} />
         <Route path="/info" element={<Info />} />
         <Route path="/areas" element={<Areas />} />
-         <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
